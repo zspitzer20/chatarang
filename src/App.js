06773c435fecb8c454bcddcbafdd3e36080d8961
Zachary Spitzer <zspitzer@nd.edu>
2018-06-12 14:02:01 -0400

    Add Main Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Chatarang</h1>
+        <Main />
       </div>
     );
   }

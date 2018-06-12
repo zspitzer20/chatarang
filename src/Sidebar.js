22d85096e6f195aaadfdabdea3cd238c9800e9f3
Zachary Spitzer <zspitzer@nd.edu>
2018-06-12 14:07:21 -0400

    Add Sidebar and Chat files

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component{
+    render(){
+        return(
+            <div className="sidebar">
+                Sidebar
+            </div>
+
+        )
+    }
+}
+
+export default Sidebar

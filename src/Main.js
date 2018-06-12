dad44324544770441ef2ee7a572aaa0397b1d522
Zachary Spitzer <zspitzer@nd.edu>
2018-06-12 14:20:43 -0400

    Styled Main

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,23 @@
 import React, { Component} from 'react'
+
 import Sidebar from './Sidebar'
 import Chat from './Chat'
+
 class Main extends Component {
     render(){
         return(
-            <div className="Main">
+            <div className="Main" style={styles}>
                 <Sidebar />
                 <Chat />
             </div>
         )
     }
+}
+
+const styles = {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
 }
 
 export default Main
